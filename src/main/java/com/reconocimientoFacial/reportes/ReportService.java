196b5785b8096d62a8a8186cb25fdad791e902c5
RodrigoVidalGuzmanDiaz <78930192+RodrigoVidalGuzmanDiaz@users.noreply.github.com>
2025-04-27 17:49:39 -0600

    Se completa el codigo para mostrar el reporte de los Accesos Generales

--- src/main/java/com/reconocimientoFacial/reportes/ReportService.java
+++ src/main/java/com/reconocimientoFacial/reportes/ReportService.java
@@ -1,34 +1,75 @@
 package com.reconocimientoFacial.reportes;
 
-import net.sf.jasperreports.engine.JREmptyDataSource;
-import net.sf.jasperreports.engine.JRException;
-import net.sf.jasperreports.engine.JasperExportManager;
-import net.sf.jasperreports.engine.JasperPrint;
+import net.sf.jasperreports.engine.*;
+import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
+import net.sf.jasperreports.engine.util.JRLoader;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ResourceLoader;
 import org.springframework.stereotype.Service;
 
 import javax.sql.DataSource;
 import java.io.InputStream;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
-
-import static net.sf.jasperreports.engine.JasperFillManager.fillReport;
 
 @Service
 public class ReportService {
-    private final DataSource dataSource;
 
-    public ReportService(DataSource dataSource) {
-        this.dataSource = dataSource;
+    @Autowired
+    private ResourceLoader resourceLoader;
+
+    @Autowired
+    private DataSource dataSource; // Inyección del DataSource configurado en application.properties
+
+    public byte[] generarReport(String reportName) throws Exception {
+        InputStream file = resourceLoader.getResource("classpath:reports/" + reportName + ".jasper").getInputStream();
+        JasperReport jasperReport = (JasperReport) JRLoader.loadObject(file);
+        Map<String, Object> parameters = new HashMap<>();
+        List<Map<String, Object>> reportData = getDataFromDatabase(); // Método para obtener los datos
+
+        JRBeanCollectionDataSource dataSource = new JRBeanCollectionDataSource(reportData);
+        JasperPrint jasperPrint = JasperFillManager.fillReport(jasperReport, parameters, dataSource);
+        return JasperExportManager.exportReportToPdf(jasperPrint);
     }
 
-    public byte [] generarReport(String reportName)  throws Exception {
-        //cargar el reporte
-        InputStream reportStream = this.getClass().getResourceAsStream("/reports/"+reportName+".jasper");
-
-        Map<String, Object> parms = new HashMap<>();
-        //Llenado
-        JasperPrint jasperPrint = fillReport(reportStream,parms, dataSource.getConnection());
-        //Exportación a un reporte de tipo PDF
-        return JasperExportManager.exportReportToPdf(jasperPrint);
+    private List<Map<String, Object>> getDataFromDatabase() throws SQLException {
+        List<Map<String, Object>> dataList = new ArrayList<>();
+        try (Connection connection = dataSource.getConnection();
+             java.sql.Statement statement = connection.createStatement();
+             java.sql.ResultSet resultSet = statement.executeQuery(
+                     "SELECT " +
+                             "    ra.id AS id_registro, " +
+                             "    u.nombre AS nombre_usuario, " +
+                             "    DATE_FORMAT(ra.fecha_hora, '%Y-%m-%d %H:%i:%s') AS fecha_hora, " +
+                             "    ra.zona_acceso, " +
+                             "    CASE ra.resultado " +
+                             "        WHEN 1 THEN 'Exitoso' " +
+                             "        WHEN 0 THEN 'Fallido' " +
+                             "        ELSE 'Desconocido' " +
+                             "    END AS estado_acceso, " +
+                             "    ra.metodo AS nombre_metodo " +
+                             "FROM " +
+                             "    db_biometria.registros_acceso AS ra " +
+                             "INNER JOIN " +
+                             "    db_biometria.usuarios AS u ON ra.id_usuario = u.id_usuario " +
+                             "ORDER BY " +
+                             "    id_registro ASC"
+             )) {
+            while (resultSet.next()) {
+                Map<String, Object> data = new HashMap<>();
+                data.put("id_registro", resultSet.getInt("id_registro"));
+                data.put("nombre_usuario", resultSet.getString("nombre_usuario"));
+                data.put("fecha_hora", resultSet.getString("fecha_hora"));
+                data.put("zona_acceso", resultSet.getString("zona_acceso"));
+                data.put("estado_acceso", resultSet.getString("estado_acceso"));
+                data.put("nombre_metodo", resultSet.getString("nombre_metodo"));
+                dataList.add(data);
+            }
+        }
+        return dataList;
     }
 }
